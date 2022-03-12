1c09e6419616606daeed9e024c077dd98f837a74
Aleksandr Vl. Matveev <aleksandr.vl.matveev@netcracker.com>
2022-03-12 14:58:23 +0300

    Add rotated labels without year

--- Visualizer.py
+++ Visualizer.py
@@ -11,6 +11,8 @@ class Visualizer:
             num_of_tasks = list(map(lambda v: len(v), self.data.values()))
             dates = list(map(lambda d: re.split(r'\d{4}-', d)[-1], self.data.keys()))
             plt.plot(dates, num_of_tasks)
+            plt.xticks(rotation=90)
+            plt.tick_params(axis='x', which='major', labelsize=5)
             plt.show()
 
     def hist(self):
